2db569301141527b6a254b7c0e10a16e59b49aed
Marcus Vinnicius <mvinniciusamorim@gmail.com>
2024-05-03 16:33:09 -0300

    atualização de value nas notas e pesos

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ let button = document.querySelector("#botao_verificar");
         button.addEventListener("click", verificar);
 
         function verificar(){
-            let n1 = document.querySelector("#n1").value;
-            let n2 = document.querySelector("#n2").value;
-            let n3 = document.querySelector("#n3").value;
-            let n4 = document.querySelector("#n4").value;
+            let n1 = document.querySelector("#n1");
+            let n2 = document.querySelector("#n2");
+            let n3 = document.querySelector("#n3");
+            let n4 = document.querySelector("#n4");
     
-            let p1 = document.querySelector("#p1").value;
-            let p2 = document.querySelector("#p2").value;
-            let p3 = document.querySelector("#p3").value;
-            let p4 = document.querySelector("#p4").value;
+            let p1 = document.querySelector("#p1");
+            let p2 = document.querySelector("#p2");
+            let p3 = document.querySelector("#p3");
+            let p4 = document.querySelector("#p4");
     
             let presenca = document.querySelector("#presenca").value;
 
-            let media = calculaMedia(n1,n2,n3,n4,p1,p2,p3,p4);
+            let media = calculaMedia(n1.value,n2.value,n3.value,n4.value,p1.value,p2.value,p3.value,p4.value);
             document.querySelector("#media").textContent = media;
 
             let aprovado = verificaAprovacao(media, presenca)
