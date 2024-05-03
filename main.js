c4738c70dfbd07b26f8b9788f3fbbf4f3acd328a
Marcus Vinnicius <mvinniciusamorim@gmail.com>
2024-05-03 16:09:22 -0300

    transfira a solução desenvolvida no arquivo html

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+
 let button = document.querySelector("#botao_verificar");
+        button.addEventListener("click", verificar);
 
-button.addEventListener("click", verificar);
+        function verificar(){
+            let n1 = document.querySelector("#n1").value;
+            let n2 = document.querySelector("#n2").value;
+            let n3 = document.querySelector("#n3").value;
+            let n4 = document.querySelector("#n4").value;
+    
+            let p1 = document.querySelector("#p1").value;
+            let p2 = document.querySelector("#p2").value;
+            let p3 = document.querySelector("#p3").value;
+            let p4 = document.querySelector("#p4").value;
+    
+            let presenca = document.querySelector("#presenca").value;
 
-function verificar() {
-  /* TODO.4: transfira a solução desenvolvida no arquivo html para cá */
-}
+            let media = calculaMedia(n1,n2,n3,n4,p1,p2,p3,p4);
+            document.querySelector("#media").textContent = media;
+
+            let aprovado = verificaAprovacao(media, presenca)
+            
+            let resultado;
+
+            if (aprovado) {
+                resultado = "Aprovado"
+            }else{
+                resultado = "Reprovado"
+            }
+
+            document.querySelector("#resultado").textContent = resultado;
+        }
