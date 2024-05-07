818a3f288cbad1fe46ea4488c5436a3aa9812258
Marcus Vinnicius <mvinniciusamorim@gmail.com>
2024-05-07 16:10:39 -0300

    colocando cor verde em aprovado e vermelho em reprovado na situação

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 
 let button = document.querySelector("#botao_verificar");
-        button.addEventListener("click", verificar);
+button.addEventListener("click", verificar);
 
-        function verificar(){
-            let n1 = document.querySelector("#n1");
-            let n2 = document.querySelector("#n2");
-            let n3 = document.querySelector("#n3");
-            let n4 = document.querySelector("#n4");
-    
-            let p1 = document.querySelector("#p1");
-            let p2 = document.querySelector("#p2");
-            let p3 = document.querySelector("#p3");
-            let p4 = document.querySelector("#p4");
-    
-            let presenca = document.querySelector("#presenca").value;
+function verificar() {
+    let n1 = document.querySelector("#n1");
+    let n2 = document.querySelector("#n2");
+    let n3 = document.querySelector("#n3");
+    let n4 = document.querySelector("#n4");
 
-            let media = calculaMedia(n1.value,n2.value,n3.value,n4.value,p1.value,p2.value,p3.value,p4.value);
-            document.querySelector("#media").textContent = media;
+    let p1 = document.querySelector("#p1");
+    let p2 = document.querySelector("#p2");
+    let p3 = document.querySelector("#p3");
+    let p4 = document.querySelector("#p4");
 
-            let aprovado = verificaAprovacao(media, presenca.value)
-            
-            let resultado;
+    let presenca = document.querySelector("#presenca");
 
-            if (aprovado) {
-                resultado = "Aprovado"
-            }else{
-                resultado = "Reprovado"
-            }
+    let media = calculaMedia(n1.value, n2.value, n3.value, n4.value, p1.value, p2.value, p3.value, p4.value).toFixed(2);
+    document.querySelector("#media").textContent = media;
 
-            document.querySelector("#resultado").textContent = resultado;
-        }
+    let aprovado = verificaAprovacao(media, presenca.value)
+
+    const element = document.querySelector('#resultado');
+
+    console.log(element)
+    if (aprovado) {
+        element.textContent = "Aprovado"
+        element.classList.add('passou');
+        element.classList.remove('reprovado');
+    } else {
+        element.textContent = "Reprovado"
+        element.classList.add('reprovado');
+        element.classList.remove('passou');
+    }
+
+}
