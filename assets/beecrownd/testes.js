d00671015d6f1e28c31e9d719e3b1c6e48e70166
Marcus Vinnicius <mvinniciusamorim@gmail.com>
2024-06-06 17:15:06 -0300

    aa

---
+++ assets/beecrownd/testes.js
@@ -0,0 +1,30 @@
+// const fs = require('fs')
+const { readFileSync } = require('fs')
+var input = readFileSync('input.txt', 'utf8');
+
+// const linhas = input.trim().split('\n')
+// let cont = 0;
+// let soma = 0;
+// for (var i = 0; i < linhas.length; i++){
+//   let a = linhas[i].split(' ')
+//   let x = Number(a[0])
+//   let y = Number(a[1])
+//   for (var j = y; j <= x; j++){
+//     soma += j
+//     console.log(j)
+//   }
+//   console.log(soma)
+// }
+// const linhas = input.split(' ');
+// let x = Number(linhas[0]) //3 
+// let y = Number(linhas[1]) //99
+
+// for (var i = 1; i <= y; i+=x){
+//   for (var j = i; j <= (x-i); j++){
+//     console.log(j)
+//   }
+  
+  
+//   console.log(i,i+1,i+2)
+// }
+
